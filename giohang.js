6da151085cef0437417946599bfc1dcfdd08e847
Anes0209 <lexuanan0209@gamil.com>
2025-12-15 17:05:33 +0700

    sua sua

--- giohang.js
+++ giohang.js
@@ -127,7 +127,7 @@ function checkout() {
             price: item.price
         })),
         totalPrice: totalPriceNumber,
-        status: "Chờ xác nhận", // Trạng thái ban đầu: "Chờ xác nhận"
+        status: 'Chờ lấy hàng', // Trạng thái ban đầu: "Chờ xác nhận"
         date: currentDate
     };
 
